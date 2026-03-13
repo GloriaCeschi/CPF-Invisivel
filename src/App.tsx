3f28fe40ec67663d702fdac04568769ae921158c
carolinegrau <carolinesgrau@gmail.com>
2026-03-13 14:26:36 -0300

    arrumei sidebar da jornada

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ import Score from "./pages/Score";
 import Perfil from "./pages/Perfil";
 import EducacaoFinanceira from "./pages/EducacaoFinanceira";
 import Cursos from "./pages/Cursos";
-import Credit from "./pages/Credit";
+import Credito from "./pages/Credito";
 import { AuthProvider } from "./context/AuthContext";
 import AuthRoute from "./components/AuthRoute";
 import ProtectedRoute from "./components/ProtectedRoute";
@@ -33,7 +33,7 @@ const App = () => (
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path='/auth' element={<AuthRoute> <Auth /> </AuthRoute>} />
-          <Route path="/credit" element={<Credit />} />
+          <Route path="/credito" element={<Credito />} />
           <Route path="*" element={<NotFound />} />
           <Route path='/home' element={<ProtectedRoute> <Home /> </ProtectedRoute>}/>
           <Route path="/profile" element={<ProtectedRoute> <CadastroUsuario /> </ProtectedRoute>} />
